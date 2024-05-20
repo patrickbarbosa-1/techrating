5de35d86f7990b4012de38773ea6379b86d1183a
Patrick Barbosa <114362691+tickbarbosa@users.noreply.github.com>
2024-05-20 19:24:54 -0300

    login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
 -->
 <html>
 	<head>
-		<title>BLOG DO MIKE OLIVEIRA</title>
+		<title>Tech Rating</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<link rel="stylesheet" href="{{url('assets/css/main.css') }}" />
